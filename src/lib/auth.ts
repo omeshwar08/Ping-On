59bd8076d36cca008c099812c1615d8a5d47d829
Omeshwar Chowdhury <63541977+code-with-omeshwar@users.noreply.github.com>
2025-10-04 11:17:08 +0530

    authentication added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import db from "./db";
-
+import { env } from "./env";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
